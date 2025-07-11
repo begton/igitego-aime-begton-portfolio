2e7d6329df4ded0bcc47be03627b23fca4b4a1a7
igitego Begton <igitegoaimebegton@gmail.com>
2025-07-11 12:55:40 +0200

    Add files via upload

---
+++ js/scrirt.js
@@ -0,0 +1,8 @@
+/*---------------------- typing animation =====================*/
+
+var typed = new Typed(".typing", {
+    strings: [,"Web Developer", "Software Developer", "Software Manager", "Database Manager"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
